cf60d7100850beb4134a3cedc375636483705f61
jackchen0120 <278910933@qq.com>
2021-09-17 08:15:16 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/jenkins.png">
+    <h2>构建伟大，无所不能</h2>
+    <h4>Jenkins是开源CI&CD软件领导者，提供超过1000个插件来支持构建、部署、自动化， 满足任何项目的需要。</h4>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  width: 300px;
+}
+</style>
