8a1e7675fd4e1d7cba344a4e8bbfb1462905f6d0
chenjian <278910933@qq.com>
2021-09-17 18:27:49 +0800

    更新内容

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/jenkins.png">
-    <h2>欢迎来到这个神奇的地方666</h2>
+    <h2>欢迎来到这个神奇的地方</h2>
     <h3>关注@我，关注@懒人码农，开启全新的体验</h3>
     <h4>Jenkins是开源CI&CD软件领导者，提供超过1000个插件来支持构建、部署、自动化， 满足任何项目的需要。</h4>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
